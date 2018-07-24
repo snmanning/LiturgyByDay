12f26bc1e0289474660a58715865adebc7440f27
Sarah Manning <sarahnmanning@gmail.com>
2018-07-23 20:10:19 -0400

    attempted to set up server.I have no idea if it works

--- src/server.js
+++ src/server.js
@@ -2,3 +2,26 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
+const server = express();
+const port = process.env.PORT || 4083;
+
+server.use(cors());
+server.use(express.static('build'));
+
+server.get('/general-en/:year/:month/:day', (requestAnimationFrame, response) => {
+    const { date } = request.params;
+    const url = `http://calapi.inadiutorium.cz/api/v0/en/calendars/general-en/${date}`;
+    axios.get(url)
+         .then(calendarResponse => {
+            response.json(calendarResponse.data);
+         })
+         .catch(() => {
+             response.status(500).json ({
+                 msg: 'Apologies, the data is not available at this time'
+             });
+         });
+});
+
+server.listen(port, () => {
+    console.log(`Now listening on port: ${port}`);
+});
